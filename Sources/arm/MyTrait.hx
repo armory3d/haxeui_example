dfa930b8bcfc04024d896ded874a434a55703844
Lubos Lenco <lubos.lenco@gmail.com>
2017-09-27 10:20:26 +0200

    Init

---
+++ Sources/arm/MyTrait.hx
@@ -0,0 +1,38 @@
+package arm;
+
+import haxe.ui.Toolkit;
+import haxe.ui.components.*;
+import haxe.ui.core.Component;
+import haxe.ui.core.Screen;
+import haxe.ui.macros.ComponentMacros;
+import haxe.ui.core.UIEvent;
+
+class MyTrait extends armory.Trait {
+
+	static var _main:Component;
+	var ready = false;
+
+	public function new() {
+		super();
+
+		notifyOnInit(function() {
+			iron.data.Data.getFont('arial.ttf', function(font:kha.Font) {
+				Toolkit.init();
+				_main = ComponentMacros.buildComponent("../../ui/main.xml");
+				Screen.instance.addComponent(_main);
+
+				var button = _main.findComponent("helloButton", null, true);
+				button.onClick = function(e:UIEvent) {
+					trace("World!");
+				}
+
+				ready = true;
+			});
+		});
+
+		iron.App.notifyOnRender2D(function(g:kha.graphics2.Graphics) {
+			if (!ready) return;
+			Screen.instance.renderTo(g);
+		});
+	}
+}
